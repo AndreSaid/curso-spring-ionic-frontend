e89edc1add61282ade318a623c15e879fe577241
Andre Said <andresaid123@hotmail.com>
2020-08-20 15:00:42 -0300

    Terminando as funcionalidades do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../../models/cart-item';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 @IonicPage()
 @Component({
@@ -19,23 +20,42 @@ export class CartPage {
     public navParams: NavParams,
     public cartService: CartService,
     public produtoService: ProdutoService) {
-    }
+  }
 
   ionViewDidLoad() {
     let cart = this.cartService.getCart();
     this.items = cart.items;
     this.loadImageUrls();
-}
-
-loadImageUrls() {
-  for (var i=0; i<this.items.length; i++) {
-    let item = this.items[i];
-    this.produtoService.getSmallImageFromBucket(item.produto.id)
-      .subscribe(response => {
-        item.produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.produto.id}-small.jpg`;
-      },
-      error => {});
   }
-} 
 
+  loadImageUrls() {
+    for (var i = 0; i < this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.produto.id)
+        .subscribe(response => {
+          item.produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.produto.id}-small.jpg`;
+        },
+          error => { });
+    }
+  }
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total() : number {
+    return this.cartService.total();
+  }  
+  
+  goOn() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
 }
